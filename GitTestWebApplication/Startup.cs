08a5229367822fc3aa9ea46f858fe413f249a6da
smharishbabu@gmail.com <4$nEhxooZ>
2015-06-01 16:28:47 +0530

    Startup File Change

--- GitTestWebApplication/Startup.cs
+++ GitTestWebApplication/Startup.cs
@@ -8,7 +8,7 @@ namespace GitTestWebApplication
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
+            ConfigureAuth(app);   
         }
     }
 }
